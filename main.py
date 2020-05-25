4574709d23eb5e70c2291db2255d16b01ed600de
stelioszlat <steliosdj6@gmail.com>
2020-05-25 22:50:22 +0300

    monitor cluster and take_decision working

--- main.py
+++ main.py
@@ -2,20 +2,10 @@
 import threading as th
 import time
 import logging
-import cluster as c
 import paramiko
+import sys
 
-logging.basicConfig(level=logging.DEBUG, format='(%(threadName)-9s) %(message)s',)
-
-def f():
-	logging.debug('thread function running')
-	return
-
-def hello():
-	print("Hello, Timer")
-
-def send_key(host, key):
-	pass
+logging.basicConfig(file='log', level=logging.DEBUG, format='(%(threadName)-9s) %(message)s')
 
 def get_server_cpu(hostname):
 	client = paramiko.SSHClient()
@@ -24,47 +14,59 @@ def get_server_cpu(hostname):
 	stdin, stdout, stderr = client.exec_command('mpstat | tail -c 6')
 
 	s = stdout.read()
-	return str(100 - float(s)
+	return str(100 - float(s))
+
 
 def send_key(hostname, key):
 	client = paramiko.SSHClient()
 	client.load_system_host_keys()
 	client.set_missing_host_key_policy()
 
+def ADD_NODE():
+	print('adding node')
+	return
+
+def REMOVE_NODE():
+	print('removing node')
+	return
+
+def TAKE_DECISION(load):
+	high = 0.8
+	low = 0.2
+
+	if load > high:
+		t1=th.Timer(1, ADD_NODE)
+		t1.start()
+		t1.join()
+	elif load < low:
+		t2=th.Timer(1, REMOVE_NODE)
+		t2.setName('remove')
+		t2.start()
+		t2.join()
 
 def MONITOR_CLUSTER():
-	s1 = th.Timer(0, get_server_cpu('voltaire')
+	s1 = get_server_cpu('montesquieu')
+	s2 = get_server_cpu('rousseau')
 
-	pass
+	m = float(s1) + float(s2) / 2
+
+	print(s1)
+	print(s2)
+
+	t = TAKE_DECISION(m)
 
 if __name__=='__main__':
 	#t = th.Timer(3.0, hello)
 	#t.start()
+	#t.setName('name')
 
-	t1 = th.Timer(5, f)
-	t1.setName('t1')
-	t2 = th.Timer(5, f)
-	t2.setName('t2')
-
-
-	logging.debug('starting timers...')
-	t1.start()
-	t2.start()
-
-
-	logging.debug('waiting before cancelling %s', t2.getName())
-	time.sleep(2)
-	logging.debug('canceling %s', t2.getName())
-	print 'before cancel t2.is_alive() = ', t2.is_alive()
-	t2.cancel()
-	time.sleep(2)
-	print 'after cancel t2.is_alive() = ', t2.is_alive()
-
-
-	t1.join()
-	t2.join()
-
-	logging.debug('done')
-
-	m =  MONITOR_CLUSTER()
-
+	try:
+		m = th.Timer(0, MONITOR_CLUSTER)
+		m.start()
+		m.join()
+		while True:
+			m = th.Timer(5, MONITOR_CLUSTER)
+			m.start()
+			m.join()
+	except(KeyboardInterrupt):
+		sys.exit(1)
